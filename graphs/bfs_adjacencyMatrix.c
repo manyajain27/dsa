ac5598ced84d82fd92ec90a0bcc339aa5b152bc0
manyajain27 <jainmanya2701@gmail.com>
2024-11-27 01:31:39 +0530

    commit

--- graphs/bfs_adjacencyMatrix.c
+++ graphs/bfs_adjacencyMatrix.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
-#define N 10
+#define N 6
 int queue[N]; 
 int front = -1, rear = -1;
 
@@ -70,31 +70,46 @@ void bfs(int graph[][N], int visited[], int start, int vertices){
 }
 
 int main(){
-    int vertices, edges, start;
+    
+    int adj[N][N] = {
+                     {0, 1, 1, 1, 0},
+                     {1, 0, 1, 0 , 1},
+                     {1, 1, 0, 1, 0},
+                     {1, 0, 1, 0, 0},
+                     {0, 1, 0, 0, 0}
+                };
+
     int visited[N];
-    printf("Enter the number of vertices: ");
-    scanf("%d", &vertices);
-    printf("Enter the number of edges: ");
-    scanf("%d", &edges);
-
-    int graph[N][N];
-    for(int i = 0; i < vertices; i++){
-        for(int j = 0; j < vertices; j++){
-            graph[i][j] = 0; //initialize the adjacency matrix to 0
-        }
-
-    }
-    for(int i = 0; i < edges; i++){
-        int u, v;
-        printf("Enter the vertices of the edge: ");
-        scanf("%d %d", &u, &v);
-        graph[u][v] = 1;
-        graph[v][u] = 1;
-    }
-
-    printf("Enter the starting vertex: ");
-    scanf("%d", &start);
-    bfs(graph, visited, start, vertices);
-
+    bfs(adj, visited, 0, N);
     return 0;
+
 }
+
+
+    // int vertices, edges, start;
+    // int visited[N];
+    // printf("Enter the number of vertices: ");
+    // scanf("%d", &vertices);
+    // printf("Enter the number of edges: ");
+    // scanf("%d", &edges);
+
+    // int graph[N][N];
+    // for(int i = 0; i < vertices; i++){
+    //     for(int j = 0; j < vertices; j++){
+    //         graph[i][j] = 0; //initialize the adjacency matrix to 0
+    //     }
+
+    // }
+    // for(int i = 0; i < edges; i++){
+    //     int u, v;
+    //     printf("Enter the vertices of the edge: ");
+    //     scanf("%d %d", &u, &v);
+    //     graph[u][v] = 1;
+    //     graph[v][u] = 1;
+    // }
+
+    // printf("Enter the starting vertex: ");
+    // scanf("%d", &start);
+    // bfs(graph, visited, start, vertices);
+
+    // return 0;
---
+++ graphs/dfs_adjacencyMatrix.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#define N 5
+int visited[5];
+
+void dfs(int graph[][5], int start)
+{
+    if (visited[start] == 1)
+    {
+        return;
+    }
+    else{
+        visited[start] = 1;
+    }
+    printf("%d ", start);
+    for (int i = 0; i < N; i++)
+    {
+        if (graph[start][i] != 0 && visited[i] == 0)
+        {
+            dfs(graph, i);
+        }
+    }
+}
+
+int main()
+{
+    
+    int graph[N][N] = {
+                     {0, 1, 1, 1, 0},
+                     {1, 0, 1, 0 , 1},
+                     {1, 1, 0, 1, 0},
+                     {1, 0, 1, 0, 0},
+                     {0, 1, 0, 0, 0}
+                };
+
+    dfs(graph, 0);
+    return 0;
+}
