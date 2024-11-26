1cfcb470c20f11682f5d5eb450c0a2af97e35cf7
manyajain27 <jainmanya2701@gmail.com>
2024-11-26 17:24:48 +0530

    queue using LL

---
+++ queue/Queue_LL.c
@@ -0,0 +1,74 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node{
+    int data;
+    struct Node *next;
+};
+
+struct Node *front=NULL;
+struct Node *rear=NULL;
+
+void enqueue(int x){
+    struct Node *temp = (struct Node *) malloc(sizeof(struct Node));
+    temp->data=x;
+    temp->next=NULL;
+
+    if(front==NULL && rear==NULL){
+        front=rear=temp;
+        return;
+    }
+    else{
+        rear->next=temp;
+        rear=temp;
+    }
+}
+
+void dequeue(){
+    struct Node *temp=front;
+
+    if(front==NULL){
+        printf("Queue is empty\n");
+        return;
+    }
+    else if(front==rear){ //one element only
+        front=rear=NULL;
+    }
+    else{
+        front=front->next;
+    }
+    free(temp);
+}
+
+void display() {
+    struct Node *temp = front;  // Start from the front
+    if (front == NULL) {
+        printf("Queue is empty\n");
+        return;
+    }
+
+    while (temp != NULL) {
+        printf("%d ", temp->data);  // Print the data
+        temp = temp->next;  // Move to the next node
+    }
+    printf("\n");
+}
+
+void peek(){
+    if(front==NULL){
+        printf("Queue is empty\n");
+        return;
+    }
+    printf("Front element is %d\n",front->data);
+}
+
+int main(){
+    enqueue(5);
+    enqueue(7);
+    enqueue(13);
+    enqueue(89);
+    dequeue();
+    display();
+    peek();
+    return 0;
+}
